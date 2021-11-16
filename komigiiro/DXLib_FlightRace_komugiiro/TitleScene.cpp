a8cbce12f922e5c5f5f0f1c7e8d5a0161810bb98
gta20-Shimizu <sumito0107@icloud.com>
2021-11-16 10:16:42 +0900

    プル用プッシュ

--- komigiiro/DXLib_FlightRace_komugiiro/TitleScene.cpp
+++ komigiiro/DXLib_FlightRace_komugiiro/TitleScene.cpp
@@ -4,6 +4,7 @@
 namespace Scene
 {
 	int num = 30;
+	double pos = 1;
 	bool flg = true;
 	void TitleScene::Calculation()
 	{
@@ -13,8 +14,10 @@ namespace Scene
 	void TitleScene::TwoDimentionsDraw()
 	{
 		num++;
+		pos++;
+		if (pos > 50) pos = -10;
 		if (num % 30 == 0) flg = flg ? false : true;
-		ui->TextCreater("ゲームプログラミング　制作", CanvasSize{0,0}, 20, "black","white",true);
+		ui->TextCreater("ゲームプログラミング　制作", CanvasSize{pos,0}, 20, "black","white",true);
 		ui->TextCreater("DXLib FlightRace!!", CanvasSize{8,15}, 50, "red", "blue", false);
 	    if (flg) ui->TextCreater("Press Enter Key - GameStart!", CanvasSize{15,35}, 20, "white");
 		ui->TextCreater("Esc - GameExit", CanvasSize{40,50}, 20, "white");
